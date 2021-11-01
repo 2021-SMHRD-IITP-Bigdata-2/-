d8925116305fc377f955da59a08f1827db5176a6
ChanI <aok458@gmail.com>
2021-11-01 17:50:43 +0900

    map 띄워짐

--- gogle/js/map.js
+++ gogle/js/map.js
@@ -1,10 +1,6 @@
 // body tag가 불러와진 후 로딩하는 함수
 // window.onload
-$(document).ready(function(){
-    initMap();
-});
-
-function initMap() {
+window.onload = function initMap() {
     // 광주 경도 위도 -> 지도가 뜨는 위치
     var gwangju = { lat: 35.1595454 ,lng: 126.8526012};
     var map = new google.maps.Map(document.querySelector('.map'), {
