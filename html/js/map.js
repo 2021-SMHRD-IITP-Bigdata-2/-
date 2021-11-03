2482c5790211129b62309ea0eccac33c5c9556c5
ChanI <aok458@gmail.com>
2021-11-03 15:28:31 +0900

    map_predict

--- html/js/map.js
+++ html/js/map.js
@@ -5,6 +5,81 @@ $(document).ready(function(){
 function close_pop(flag) {
     $('#myModal').hide();
 }
+
+// 이미지 업로드 기능
+const browseBtn = document.querySelector('.browse-btn');
+const realInput = document.querySelector('.hidden-input')
+
+browseBtn.addEventListener('click', function(){
+    realInput.click();
+});
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('.image-upload-wrap').hide();
+            $('.file-upload-image').attr('src', e.target.result);
+            $('.file-upload-content').show();
+            $('.image-title').html(input.files[0].name);
+        };
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        removeUpload();
+    }
+}
+
+function removeUpload() {
+    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+    $('.file-upload-content').hide();
+    $('.image-upload-wrap').show();
+}
+$('.image-upload-wrap').bind('dragover', function () {
+    $('.image-upload-wrap').addClass('image-dropping');
+});
+$('.image-upload-wrap').bind('dragleave', function () {
+    $('.image-upload-wrap').removeClass('image-dropping');
+});
+
+// 예측 코드
+const URL = "https://teachablemachine.withgoogle.com/models/MirojN-2M/";
+let model, labelContainer, maxPredictions;
+// Load the image model and setup the webcam
+async function predict() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+    labelContainer = document.getElementById("label-container");
+
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
+        labelContainer.appendChild(document.createElement("div"));
+
+    }
+
+    var image = document.getElementById("face-image");
+    const prediction = await model.predict(image, false);
+    var maxpre = [];
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+            console.log(prediction[i].className)
+            maxpre.push(prediction[i].probability)
+        // labelContainer.childNodes[i].innerHTML = classPrediction;
+    }
+    console.log(maxpre);
+    const max = maxpre.indexOf(Math.max(...maxpre));
+    console.log(max);
+    labelContainer.innerHTML = prediction[max].className;
+
+    $('#myModal').hide();
+
+}
+
 // 위치 표기
 var locations = [
     ['동원 게장 백반', 35.1491293, 126.9424985],
