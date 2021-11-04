1157f76e47cbbd95adf83bd342f0b33d308c2bf3
teemostock00 <teemostock00@gmail.com>
2021-11-04 11:07:44 +0900

    server 정리

--- Server/server.js
+++ Server/server.js
@@ -4,39 +4,21 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
-const mysql = require("mysql");
-const dotenv = require('dotenv');
+const ejs = require('ejs')
+const cors = require('cors');
 // Google Auth start
 const {OAuth2Client} = require('google-auth-library');
 const CLIENT_ID = '328822197094-khiejtot9evtjn0tmd24b3ppo14io05e.apps.googleusercontent.com'
 const client = new OAuth2Client(CLIENT_ID);
 // Google Auth end
 // 모듈 end
+// mysql 가져오기
+const db = require('./routes/connection.js');
 
-// .env 파일 사용
-dotenv.config({ path: './.env'})
+
 
 // 포트 5000번 설정
-const PORT = process.env.PORT || 5000;
-
-// mysql 설정 start
-// .env 파일에 깃허브 푸쉬할때 보안때문에 설정해놓음
-const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
-});
-// 연결했을때 에러뜨면 콘솔에 띄워야 에러가 확인됨
-db.connect( (error) => {
-    if(error){
-        console.log(error)
-    } else{
-        console.log("MYSQL Connected.....")
-    }
-});
-// mysql 설정 end
-
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 // ejs 사용
@@ -46,6 +28,9 @@ app.use(express.static('public'));
 app.use(express.json());
 // cookieParser는 사용자가 인증되었는지 확인하고 특정 경로에 엑세스 할 수 있는지 여부를 결정하기 위해 쿠키 사용
 app.use(cookieParser());
+// 로그인 회원가입 라우터 api 경로로 들어갈 수 있음
+const router = require("./routes/router.js");
+app.use('/api', router);
 
 // <--                                 구글 로그인 start                                  -->
 // login.ejs 렌더링
@@ -53,7 +38,6 @@ app.get('/login', (req, res) => {
     res.render('login')
     console.log('login');
 });
-
 app.post('/login', (req, res) => {
     let token = req.body.token;
     console.log(token);
@@ -77,16 +61,13 @@ verify()
     res.send('success');
     }).catch(console.error);
 });
-
-// app.get('/index', (req, res) => {
-//     let user = req.user;
-//     res.render('index', {user});
-// });
-
+app.get('/index', (req, res) => {
+    let user = req.user;
+    res.render('index', {user});
+});
 // app.get('/protectedroute', (req, res) => {
 //     res.render('protectedroute');
 // });
-
 function checkAuthenticated(req, res, next){
 
     let token = req.cookies['session-token'];
@@ -110,20 +91,18 @@ function checkAuthenticated(req, res, next){
         .catch(err=>{
             res.redirect('/login')
         })
-
 }
 // <--                                 구글 로그인 end                                 -->
-
 
 // app.get('/protectedroute', (req, res) => {
 //     res.render('protectedroute');
 // });
 
 // index.ejs 렌더링해서 화면 보여줌
-app.get('/', (req, res) => {
-    res.render('index');
-    console.log('index');
-});
+// app.get('/', (req, res) => {
+//     res.render('index');
+//     console.log('index');
+// });
 
 app.get('/drink', (req, res) => {
     res.render('drink');
